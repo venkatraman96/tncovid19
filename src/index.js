242b2deb560277031652be069327791835ce2a4a
Venkatraman Muthukrishnan <madurai.mvr@gmail.com>
2020-03-26 00:13:03 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -7,25 +7,18 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 // core components
 import Covid from "layouts/Covid.js";
 
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import store from "./store/config/store";
 
 import "assets/css/material-dashboard-react.css?v=1.8.0";
 
 const hist = createBrowserHistory();
 
 export const rootComponent = (
-  <Provider store={store}>
-    <BrowserRouter>
     <Router history={hist}>
     <Switch>
       <Route path="/covid" component={Covid} />
       <Redirect from="/" to="/covid/dashboard" />
     </Switch>
   </Router>
-    </BrowserRouter>
-  </Provider>
 );
 
 ReactDOM.render(
